433a5121948407ef69abe53b9940d94f7ffdd504
ormon <armnvrma10@gmail.com>
2025-08-30 20:36:20 +0530

    take fixed

--- programs/escrow_anchor/src/lib.rs
+++ programs/escrow_anchor/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 mod state;
 mod errors;
 mod instructions;
-use instructions::*;
+// use instructions::*;
 
 declare_id!("22222222222222222222222222222222222222222222");
 
